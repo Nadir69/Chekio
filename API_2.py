3adf38a2a24cdf329b88a4c0ced2e56b300b68a1
Nadir69 <luciefel@yandex.ru>
2015-06-29 17:29:26 +0300

    Delete API_2.py

--- API_2.py
+++
@@ -1,9 +0,0 @@
-__author__ = 'v_shabalin'
-import requests
-
-request = requests.get('http://eu.wgnwn.wgns.iv/rp/api/accounts/3001001263/')
-print 'request:', request
-
-response = requests.get('http://eu.wgnwn.wgns.iv/rp/api/accounts/3001001263/')
-print 'response status code:', response.status_code
-print 'response headers:', response.headers
