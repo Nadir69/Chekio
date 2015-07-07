260928d417bbb2d91f21f32663607386d741a9e4
v_shabalin <v_shabalin@qa.wargaming.net>
2015-07-07 10:28:35 +0300

    Absolute sorting

--- Absolute sorting.py
+++ Absolute sorting.py
@@ -1,4 +1,4 @@
 def checkio(numbers_array):
-    return sorted(numbers_array, key=lambda x,y : abs(x) > x)
+    return sorted(numbers_array, key=abs)
 
-print checkio((-20, -5, 10, 15))
+print checkio((-1, -2, -3, 0))
