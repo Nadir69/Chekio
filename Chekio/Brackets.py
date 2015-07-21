8e9d3fd0ac6cb4f1ede3fae689c26e799fdb25a0
v_shabalin <v_shabalin@qa.wargaming.net>
2015-07-21 18:09:10 +0300

    Xs Os + speech module

--- Chekio/Brackets.py
+++ Chekio/Brackets.py
@@ -4,10 +4,27 @@ import string
 
 def checkio(expression):
     chars = string.digits + '+-*/'
-#    expression = list(expression)
-    for item in range(len(expression)):
-        expression = expression.replace(chars[item], '')
-    return expression
+    a = ['(', ')']
+    b = ['{', '}']
+    c = ['[', ']']
+    for item in chars:
+        expression = expression.replace(item, '')
+    count = 0
+    while count < len(expression):
+        if expression[count] == a[1] and expression[count-1] == a[0]:
+            expression = expression[:count-1] + expression[count+1:]
+            count = 0
+        elif expression[count] == b[1] and expression[count-1] == b[0]:
+            expression = expression[:count-1] + expression[count+1:]
+            count = 0
+        elif expression[count] == c[1] and expression[count-1] == c[0]:
+            expression = expression[:count-1] + expression[count+1:]
+            count = 0
+        else:
+            count += 1
+    if len(expression) == 0:
+        return True
+    else:
+        return False
 
-
-print checkio("((5+3)*2+1)")
+print checkio("[{(((((({[[{{(({()}))}}]]}))))))}]")
