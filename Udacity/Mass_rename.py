9e3e4e99bb4621e65308cc78e27270b1df789e39
merlo <luciefeL@yandex.ru>
2016-01-17 21:23:33 +0300

    Mass rename completed

--- Udacity/Mass_rename.py
+++ Udacity/Mass_rename.py
@@ -1,3 +1,9 @@
 import os
 
 file_list = os.listdir('/home/merlo/Desktop/prank/')
+print file_list
+os.chdir('/home/merlo/Desktop/prank/')
+for file_name in file_list:
+    os.rename(file_name, file_name.translate(None, "1234567890"))
+file_list = os.listdir('/home/merlo/Desktop/prank/')
+print file_list
